d27ab53f99a307ff11b361274b5ab3b688738c8a
Mohammedtariq97 <68746178+Mohammedtariq97@users.noreply.github.com>
2020-09-14 20:01:04 +0530

    Searching_algo added

---
+++ Searching_algorithms/src/Linearsearch/linearsearch.java
@@ -0,0 +1,22 @@
+package algo.linearsearch;
+
+public class App{
+	
+	public static void main(String [] args) {
+		
+		System.out.println(linearSearch(new int[] {2,4,6,7,12,15,17}, 6));
+	}
+
+public static int linearSearch(int[] a, int x) {
+	
+		int answer = -1;
+		for(int i=0; i<a.length;i++) {
+			
+			if(a[i]==x) {
+				answer = i;
+			}
+		}
+		return answer;
+	}
+
+}
---
+++ Searching_algorithms/src/RecursiveBinarySearch/RecursiveBinarySearch.java
@@ -0,0 +1,32 @@
+package algo.recursiveBinarySearch;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		System.out.println(recursiveBinarySearch(new int[] { 1, 2, 3, 4, 6, 8, 12, 18 }, 0, 7, 18));
+
+	}
+
+	public static int recursiveBinarySearch(int[] a, int p, int r, int x) {
+		
+	if(p>r) {
+		return -1;
+	}else {
+		
+		int q=(p+r)/2;
+		
+		if(a[q]==x) {
+			return q;
+		}else if(a[q]>x) {
+			return recursiveBinarySearch(a, p, q-1,x);
+		}else {
+			return recursiveBinarySearch(a, q+1, r,x);
+		}
+		
+	}
+
+	
+}
+
+}
