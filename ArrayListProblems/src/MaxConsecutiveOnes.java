aea87d30a8aebb097660dda85baea138ce2bef91
Mohammedtariq97 <68746178+Mohammedtariq97@users.noreply.github.com>
2021-06-01 11:46:34 +0530

    move zeroes updated

--- ArrayListProblems/src/MaxConsecutiveOnes.java
+++ ArrayListProblems/src/MaxConsecutiveOnes.java
@@ -1,4 +1,17 @@
 
 public class MaxConsecutiveOnes {
-
+	public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int resCount = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == 1){
+                count++;
+        
+            }else{
+                resCount = Math.max(resCount,count);
+                count =0;
+            }
+        }
+        return Math.max(resCount,count);
+    }
 }
--- ArrayListProblems/src/MoveZeroes.java
+++ ArrayListProblems/src/MoveZeroes.java
@@ -1,4 +1,19 @@
 
 public class MoveZeroes {
-
+	  public void moveZeroes(int[] nums) {
+	        int[] res = new int[nums.length];
+	        int j=0;
+	        for(int i=0;i<nums.length;i++){
+	            if(nums[i] != 0){
+	                res[j] = nums[i];
+	                j++;
+	            }
+	        }
+	        for(int i=j;i<nums.length;i++){
+	            res[i] = 0;
+	        }
+	        for(int i=0;i<nums.length;i++){
+	            nums[i] = res[i];
+	        }
+	    }
 }
