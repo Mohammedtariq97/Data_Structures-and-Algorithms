97e53f720ade11e94d5404b5e28fc674ee8a290d
Mohammedtariq97 <68746178+Mohammedtariq97@users.noreply.github.com>
2021-06-02 11:35:50 +0530

    Remove duplicates and remove elements updated

--- ArrayListProblems/src/RemoveElement.java
+++ ArrayListProblems/src/RemoveElement.java
@@ -1,4 +1,17 @@
 
 public class RemoveElement {
-
+	//removed element in place
+	public int removeElement(int[] nums, int val) {
+        int i=0;
+        int j= nums.length;
+        while(i<j){
+            if(nums[i] == val){
+                nums[i]= nums[j-1];
+                j--;
+            }else{
+                i++;
+            }
+        }
+        return j;
+    }
 }
