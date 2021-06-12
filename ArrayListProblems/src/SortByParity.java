021632ed39a3db0c8a5cc5a0ac1370f41cb1c83c
Mohammedtariq97 <68746178+Mohammedtariq97@users.noreply.github.com>
2021-06-12 19:16:08 +0530

    Sort by parity

---
+++ ArrayListProblems/src/SortByParity.java
@@ -0,0 +1,20 @@
+
+public class SortByParity {
+	 public int[] sortArrayByParity(int[] nums) {
+	        int[] res = new int[nums.length];
+	        int j=0;
+	        for(int i=0;i<nums.length;i++){
+	            if(nums[i]%2 == 0){
+	                res[j] = nums[i];
+	                j++;
+	            }
+	        }
+	        for(int i=0;i<nums.length;i++){
+	            if(nums[i]%2 != 0){
+	                res[j] = nums[i];
+	                j++;
+	            }
+	        }
+	        return res;
+	    }
+}
