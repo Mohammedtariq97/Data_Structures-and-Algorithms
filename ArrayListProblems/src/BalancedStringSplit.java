daeae77b49ebbdf1845dc3d833daa30867396cd5
Mohammedtariq97 <68746178+Mohammedtariq97@users.noreply.github.com>
2021-06-16 12:43:38 +0530

    Balanced String split

---
+++ ArrayListProblems/src/BalancedStringSplit.java
@@ -0,0 +1,21 @@
+
+public class BalancedStringSplit {
+	public int balancedStringSplit(String s) {
+        int cR = 0;
+        int cL = 0;
+        int count =0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == 'R'){
+                cR++;
+            }else{
+                cR--;
+            }
+            if(cR == cL){
+                count++;
+                cR =0;
+                cL =0;
+            }
+        }
+        return count;
+    }
+}
