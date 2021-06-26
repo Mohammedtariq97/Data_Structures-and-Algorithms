dbe9dad47631ca68608e9b31dd501d8695eea835
Mohammedtariq97 <68746178+Mohammedtariq97@users.noreply.github.com>
2021-06-26 16:16:13 +0530

    Moving average with reduced complexity using circular queue

--- QueueProblems/src/SlidingWindow.java
+++ QueueProblems/src/SlidingWindow.java
@@ -5,13 +5,36 @@ public class SlidingWindow {
 
 	public static void main(String[] args) {
 	
-		MovingAverage movingAverage = new MovingAverage(3);
+		MovingAverage1 movingAverage = new MovingAverage1(3);
 		movingAverage.next(1); // return 1.0 = 1 / 1
 		movingAverage.next(10); // return 5.5 = (1 + 10) / 2
 		movingAverage.next(3); // return 4.66667 = (1 + 10 + 3) / 3
 		movingAverage.next(5); // return 6.0 = (10 + 3 + 5) / 3
 		System.out.println(movingAverage.next(5)); //return 4.33 = (3 + 5 + 5) / 3
 	}
+}
+
+class MovingAverage1 {
+    int size, head = 0, windowSum = 0, count = 0;
+    int[] queue;
+  public MovingAverage1(int size) {
+    this.size = size;
+    queue = new int[size];
+  }
+
+  public double next(int val) {
+    ++count;
+    
+    //calculating new sum by shifting((previous sum - old element) + new element)
+    int tail = (head + 1) % size;
+    windowSum = windowSum - queue[tail] + val;
+    
+    // moving to next head
+    head = (head + 1) % size;
+    queue[head] = val;
+    //calculating average
+    return windowSum * 1.0 / Math.min(size, count);
+  }
 }
 
 class MovingAverage {
