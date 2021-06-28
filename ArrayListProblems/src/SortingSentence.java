3b695a54ebadda9f08b82c5078dc507598e8a8a3
Mohammedtariq97 <68746178+Mohammedtariq97@users.noreply.github.com>
2021-06-28 17:45:49 +0530

    Sorting sentence

---
+++ ArrayListProblems/src/SortingSentence.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class SortingSentence {
+
+	public static void main(String[] args) {
+		System.out.println(sortSentence("is2 sentence4 This1 a3"));
+
+	}
+	
+	public static String sortSentence(String s) {
+        String[] str = null;
+        str = s.split(" ");
+        int len = str.length;
+        String[] str2 = new String[len];
+        for(int i=0;i<str.length;i++){
+            String st = str[i];
+            char c = st.charAt(st.length() -1);
+            int a = Character.getNumericValue(c);
+            String formattedString = str[i].replaceAll("[^a-zA-Z]","");
+            str2[a-1] = formattedString;
+        }
+        String s1 = Arrays.toString(str2);
+        String s2 = s1.replaceAll("\\[","").replaceAll("\\]","").replaceAll(",","");
+        return s2;
+    }
+
+}
