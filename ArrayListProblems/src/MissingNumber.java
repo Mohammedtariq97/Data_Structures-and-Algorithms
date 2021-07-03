70abd4de4723f2d458981cff9832d8c08778e489
Mohammedtariq97 <68746178+Mohammedtariq97@users.noreply.github.com>
2021-07-03 19:39:34 +0530

    Missing number

---
+++ ArrayListProblems/src/MissingNumber.java
@@ -0,0 +1,22 @@
+
+public class MissingNumber {
+	 public int missingNumber(int[] nums) {
+	        HashMap<Integer,Integer> map = new HashMap<>();
+	        int min = 0;
+	        int max = 0;
+	        for(int i=0;i<nums.length;i++){
+	            min = Math.min(min,nums[i]);
+	            max = Math.max(max,nums[i]);
+	        }
+	        for(int i=0;i<nums.length;i++){
+	            map.put(nums[i],i);
+	        }
+	        for(int i=min;i<=max;i++){
+	            if(!map.containsKey(i)){
+	                return i;
+	            }
+	        }
+	        
+	        return -1;
+	    }
+}
