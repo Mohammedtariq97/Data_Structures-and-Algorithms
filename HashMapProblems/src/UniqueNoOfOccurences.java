8c525f7a4b590951f3e9db1d04c53192e3efe2e3
Mohammedtariq97 <68746178+Mohammedtariq97@users.noreply.github.com>
2021-07-06 12:31:12 +0530

    Unique no of occurences

---
+++ HashMapProblems/src/UniqueNoOfOccurences.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class UniqueNoOfOccurences {
+	public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        HashMap<Integer,Integer> map1 = new HashMap<>();
+        
+        for(int a:arr){
+            map.put(a,map.getOrDefault(a,0)+1);
+        }
+        
+        for(int n:map.values()){
+            if(map1.containsKey(n)){
+                return false;
+            }else{
+                map1.put(n,0);
+            }
+        }
+        return true;
+    }
+}
