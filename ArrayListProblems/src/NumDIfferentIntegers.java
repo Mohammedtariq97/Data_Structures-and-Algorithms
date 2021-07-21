a28f9e62198f4f90990d79067f8b94982f1bff48
Mohammedtariq97 <68746178+Mohammedtariq97@users.noreply.github.com>
2021-07-21 20:50:53 +0530

    NumDifferent integers

---
+++ ArrayListProblems/src/NumDIfferentIntegers.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class NumDIfferentIntegers {
+	 public int numDifferentIntegers(String word) {
+	        String[] arr= word.split("\\D");
+	        Set<String> s = new HashSet<>();
+	        for(String str:arr){
+	            str=str.trim();
+	            if(!str.equals("")){
+	                str = str.replaceAll("^0*","");
+	                s.add(str);
+	            }
+	        }
+	        return s.size();
+	    }
+}
