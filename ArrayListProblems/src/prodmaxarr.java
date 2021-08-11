bfba6d1f5eed06ae47edb7ffafc1c99b0e53cff1
Mohammedtariq97 <68746178+Mohammedtariq97@users.noreply.github.com>
2021-08-11 19:56:54 +0530

    product max.array

---
+++ ArrayListProblems/src/prodmaxarr.java
@@ -0,0 +1,17 @@
+
+public class prodmaxarr {
+	public int maxProduct(int[] nums) {
+        int result =0;
+       int endResult =0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                int a = (nums[i]-1) * (nums[j]-1);
+                result = Math.max(a,result);
+            }
+            endResult = Math.max(endResult,result);
+            result =0;
+        }
+        
+        return endResult;
+    }
+}
