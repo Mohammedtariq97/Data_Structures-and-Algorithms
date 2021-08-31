10414b2504e17ed44b356bf485ce813b9037a6c2
Mohammedtariq97 <ms.tariq97@gmail.com>
2021-08-31 10:44:04 +0530

    Pangram Sentence

---
+++ HashMapProblems/src/PangramSentence.java
@@ -0,0 +1,11 @@
+public boolean checkIfPangram(String sentence) {
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<sentence.length();i++){
+            char c = sentence.charAt(i);
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        if(map.size() == 26){
+            return true;
+        }
+        return false;
+}
