a426c0b1bfa72bdcc5843d19e955138f58c17d8d
Mohammedtariq97 <ms.tariq97@gmail.com>
2021-09-20 19:27:25 +0530

    Delete element from array in place

---
+++ ArrayProblems/src/DeletingElement.java
@@ -0,0 +1,17 @@
+
+public class ElementDeleting {
+	//deleted element in place
+	public int removeElement(int[] nums, int val) {
+        int i=0;
+        int j= nums.length;
+        while(i<j){
+            if(nums[i] == val){
+                nums[i]= nums[j-1];
+                j--;
+            }else{
+                i++;
+            }
+        }
+        return j;
+    }
+}
