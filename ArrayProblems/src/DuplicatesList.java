639925919b8549f5cdf04b4d2e9a1740df64dad5
Mohammedtariq97 <ms.tariq97@gmail.com>
2021-10-26 19:25:34 +0530

    Counting unique values

---
+++ ArrayProblems/src/DuplicatesList.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DuplicatesList {
+	public List<Integer> findDuplicates(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int n:nums){
+            if(map.containsKey(n)){
+                list.add(n);
+            }else{
+                map.put(n,0);
+            }
+        }      
+        return list;
+    }
+}
