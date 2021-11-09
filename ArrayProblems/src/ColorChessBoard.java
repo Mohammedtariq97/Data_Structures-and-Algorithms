e69a1fb31b5a4004b1becb1e9a43181e58d0136a
Mohammedtariq97 <ms.tariq97@gmail.com>
2021-11-09 11:42:03 +0530

    Chess problem & self-dividing numbers

---
+++ ArrayProblems/src/ColorChessBoard.java
@@ -0,0 +1,14 @@
+public boolean squareIsWhite(String coordinates) {
+        if(coordinates.charAt(0)%2 != 0 && coordinates.charAt(1)%2 != 0){
+            return false;
+        }else if(coordinates.charAt(0)%2 == 0 && coordinates.charAt(1)%2 == 0){
+            return false;
+        }else if(coordinates.charAt(0)%2 != 0 && coordinates.charAt(1)%2 == 0){
+            return true;
+        }else if(coordinates.charAt(0)%2 == 0 && coordinates.charAt(1)%2 != 0){
+            return true;
+        }
+        return false;
+    }
+
+	
